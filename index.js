67bf6928a97d55f8340f8a4389363e9cce20dcb5
Tiffany <tiffany.chen@baifu-tech.net>
2019-10-26 17:35:56 +0800

    [MOD]

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ let server = app.listen(myPort, () => {
   console.log('Your App is running at http://localhost:' + myPort);
 })
 
-
 let baseInfo = [];
 
 const getNeedData = () => {
@@ -29,36 +28,37 @@ const getNeedData = () => {
           // 取得各股票最新基本資訊
           baseInfo = [];
           let currentCount = 0;
-          stockList.filter((a, i) => i < 5).forEach((stockInfo, idx) => {
-            fetchStockInfo(stockInfo.code).then(() => {
-              currentCount++;
-              if (currentCount === stockList.filter((a, i) => i < 5).length) {
-                console.log(baseInfo);
-                resolve(baseInfo)
-              }
-            }, error => reject(error))
+          stockList.forEach((stockInfo, idx) => {
+            setTimeout(function () {
+              fetchStockInfo(stockInfo).then(() => {
+                currentCount++;
+                if (currentCount === stockList.length) {
+                  console.log(baseInfo);
+                  resolve(baseInfo)
+                }
+              }, error => reject(error))
+            }, idx * 500);
           })
         }
       });
   })
 }
 
-const fetchStockInfo = (code) => {
+const fetchStockInfo = (stockInfo) => {
   return new Promise((resolve, reject) => {
-    // setTimeout(function () {
+    console.log(`get stock ${stockInfo.code}...`);
     superagent
-      .get(`https://goodinfo.tw/StockInfo/StockFinDetail.asp?RPT_CAT=IS_M_QUAR_ACC&STOCK_ID=${code}`)
+      .get(`https://goodinfo.tw/StockInfo/StockFinDetail.asp?RPT_CAT=IS_M_QUAR_ACC&STOCK_ID=${stockInfo.code}`)
       .set({ "Content-Type": "application/json", "Accept": "application/json", 'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36' })
       .end((err2, res2) => {
         if (err2) {
-          reject(`抓取失敗 - ${err}`);
+          reject(`抓取 ${stockInfo.code} 失敗 - ${err2}`);
           return;
         } else {
-          baseInfo.push(getBaseInfo(res2));
+          baseInfo.push(getBaseInfo(res2, stockInfo));
           resolve();
         }
       });
-    // }, 500);
   })
 }
 
@@ -70,6 +70,7 @@ const getListedCompanyInfoList = (res) => {
     const data = {};
     const first_td = table_tr.eq(i).find('td');
     data.code = first_td.eq(0).text().split('　')[0];
+    if (data.code === ' 上市認購(售)權證  ') break;
     data.name = first_td.eq(0).text().split('　')[1];
     data.listingDate = first_td.eq(2).text();
     data.industry = first_td.eq(4).text();
@@ -79,14 +80,15 @@ const getListedCompanyInfoList = (res) => {
   return list;
 }
 
-const getBaseInfo = (res) => {
+const getBaseInfo = (res, stockInfo) => {
   let $ = cheerio.load(res.text);
   let data = {};
   const table_tr_1 = $("table.std_tbl tbody tr:nth-child(1) td.head_td table.none_tbl tr:nth-child(1)");
+  Object.keys(stockInfo).forEach(infoKey => {
+    data[infoKey] = stockInfo[infoKey];
+  })
   for (let i = 0; i < table_tr_1.length; i++) {
     const table_td = table_tr_1.eq(i).find('td');
-    data.code = table_td.eq(0).text().substring(0, 4);
-    data.name = table_td.eq(0).text().substring(5, table_td.eq(0).text().length);
     data.priceEvaluation = table_td.eq(1).text();
     data.PBREvaluation = table_td.eq(2).text();
     data.date = table_td.eq(3).text().substr(6);
